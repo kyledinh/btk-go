4b4f9f03f1617ce49939e6fd759b7b18e81a2af8
Kyle Dinh <kyledinh@gmail.com>
2022-07-11 08:49:47 -0700

    http-server for embedded FSs

--- cmd/http-server/main.go
+++ cmd/http-server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/kyledinh/btk-go/pkg/codex"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -23,6 +25,9 @@ func main() {
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	http.Handle("/docs/", http.FileServer(http.FS(codex.DocsFS)))
+	http.Handle("/snippets/", http.FileServer(http.FS(codex.SnippetsFS)))
+	http.Handle("/templates/", http.FileServer(http.FS(codex.TemplatesFS)))
 
 	fmt.Printf("Starting HTTP server on port %s.\n", "8001")
 	http.ListenAndServe(":8001", nil)
